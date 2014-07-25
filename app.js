87ef87aa10da3b3cfeee3d4a3fddd95de400294b
unknown <crowat@.eni.local>
2014-07-25 13:34:05 -0600

    Added some parsing of the score/driver files and actually populating things on the API calls

--- app.js
+++ app.js
@@ -2,11 +2,48 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var parseArgs = require('minimist');
+var _ = require('underscore');
 var router = express.Router();
 var converter = require('csvtojson').core.Converter;
 
+var filePath = 'd:\\mjtiming\\mjtiming\\eventdata\\';
+var csvPath = filePath + 'test.csv';
+var scoreData = {};
 
-var csvPath = 'd:\\test.csv';
+var addScores = function(scores, prefix) {
+  scoreData[prefix] = scores;
+};
+
+var parseScoreFiles = function(prefix) {
+  var driverFileStream = fs.createReadStream(filePath + prefix + '_driverData.csv');
+  var driverCsvConverter = new converter({});
+  driverCsvConverter.on("end_parsed", function(driverJsonObj) {
+    var path = filePath + prefix + '_timingData.csv';
+    var scoresCsvConverter = new converter({});
+    var scoresFileStream = fs.createReadStream(path, {flags: 'r'});
+    scoresFileStream.on('error', function(error) {
+      addScores({Drivers: driverJsonObj}, prefix);
+    });
+    scoresFileStream.on('readable', function() {
+      scoresCsvConverter.on("end_parsed", function(scoresJsonObj) {
+        addScores({Drivers: driverJsonObj, Scores: scoresJsonObj}, prefix);
+      });
+      scoresFileStream.pipe(scoresCsvConverter);
+    });
+  });
+  driverFileStream.pipe(driverCsvConverter);
+};
+
+var refreshScoreData = function() {
+  var matchRegex = new RegExp('_driverData.csv$');
+  _.each(_.map(_.filter(fs.readdirSync(filePath), function(path) {
+         return matchRegex.test(path);
+      }), function(path) {
+       return path.replace(/_driverData.csv$/,'');
+    }), function(prefix) {
+     parseScoreFiles(prefix);
+   });
+};
 // Pull port from command line argument, if present
 port = parseArgs(process.argv.slice(2))['port'] || 80;
 
@@ -14,17 +51,15 @@ router.use(function(req,res,next) {
   next();
 });
 router.get('/scorelist', function(req, res, next) {
-  var scoreDates = ['2014_01_01', '2014_02_02', '2014_03_03'];
-  res.send(scoreDates);
+  res.send(_.map(_.keys(scoreData), function(key) { return {Prefix: key, Has: _.keys(scoreData[key])};}));
 });
 router.get('/scoresbyprefix', function(req, res, next) {
-  res.sendfile(csvPath);
-  /*var fileStream = fs.createReadStream(csvPath);
-  var csvConverter = new Converter({});
-  csvConverter.on("end_parsed", function(jsonObj) {
-    res.send(jsonObj);
-  });
-  fileStream.pipe(csvConverter);*/
+  var prefix=req.query.prefix;
+  if (_.isUndefined(req.query.prefix) || _.isUndefined(scoreData[prefix])) {
+    res.send({});
+  } else {
+  res.send(scoreData[prefix]);
+  }
 });
 
 var app = express();
@@ -33,3 +68,4 @@ app.use("/lib", express.static(path.join(__dirname, 'bower_components')));
 app.use("/scores", router);
 app.use("/", express.static(path.join(__dirname,'public')));
 app.listen(port);
+refreshScoreData();
