c0c2e564f80ffad84313ba10a17b4930aab92474
unknown <crowat@.eni.local>
2014-07-25 08:03:57 -0600

    Some more work on the express framework bits, added more columns to the display to get a feel for the data that's going in there, added csvtojson dependency however it's not working in the router properly

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var fs = require('fs');
 var express = require('express');
 var parseArgs = require('minimist');
 var router = express.Router();
- 
+var converter = require('csvtojson').core.Converter;
+
+
+var csvPath = 'd:\\test.csv';
 // Pull port from command line argument, if present
 port = parseArgs(process.argv.slice(2))['port'] || 80;
 
@@ -11,28 +14,22 @@ router.use(function(req,res,next) {
   next();
 });
 router.get('/scorelist', function(req, res, next) {
-  res.send('score list');
+  var scoreDates = ['2014_01_01', '2014_02_02', '2014_03_03'];
+  res.send(scoreDates);
 });
 router.get('/scoresbyprefix', function(req, res, next) {
-  res.send('scores by prefix');
+  res.sendfile(csvPath);
+  /*var fileStream = fs.createReadStream(csvPath);
+  var csvConverter = new Converter({});
+  csvConverter.on("end_parsed", function(jsonObj) {
+    res.send(jsonObj);
+  });
+  fileStream.pipe(csvConverter);*/
 });
 
 var app = express();
 
-
 app.use("/lib", express.static(path.join(__dirname, 'bower_components')));
 app.use("/scores", router);
 app.use("/", express.static(path.join(__dirname,'public')));
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 app.listen(port);
--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,12 @@ angular.module('myApp', [])
     return time.toFixed(3);
   };
 })
+.filter('boolformatter', function() {
+  return function(value) {
+    if (value) return 'Yes';
+    return 'No';
+  };
+})
 .controller('appController', function ($scope, $http) {
   // on page load make the http request to get score data
   // process it - times should maybe be total in ms, we can use a formatter on them later
@@ -18,10 +24,10 @@ angular.module('myApp', [])
   // we could collapse or remove some columns when viewed vertically, perhaps car could be removed for this view
   $scope.text = "Hello world!";
   $scope.data = [
-    { driver:"Bob2", number: 5, time: 99050},
-    { driver:"Bob", number: 1, time: 91050},
-    { driver:"Jim", number: 99, time: 100358},
-    { driver:"Frank", number: 51, time: 30123}
+    { driver:"Bob2", number: 5, time: 99050, class: 'STF', car:'Some death trap',member:true,rookie:false},
+    { driver:"Bob", number: 1, time: 91050, class:'SSM', car:'Another death trap',member:false,rookie:true},
+    { driver:"Jim", number: 99, time: 100358, class:'BSP', car:'Cheater-mobile',member:true,rookie:true},
+    { driver:"Frank", number: 51, time: 30123, class:'AM', car:'Cheater killer', member:false,rookie:false}
   ];
   $scope.hasData = function() {
     return !_.isUndefined($scope.data) && $scope.data.length > 0;
